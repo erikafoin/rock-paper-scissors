a8ab5b049328c4792752056aa36209880d7bc87b
Erika Foin <erika.foin@gmail.com>
2019-10-02 21:17:36 -0700

    get-random-throw improvements, not working yet

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,23 +1,20 @@
 
-/*const rockValue = 0;
+const rockValue = 0;
 const paperValue = 1;
-const scissorsValue = 2;*/
-
-function getOneTwoThree(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  console.log(getOneTwoThree(3));
+const scissorsValue = 2;
   
 
-  const getRandomThrow = (getOneTwoThree) => {
-        if (getOneTwoThree === 0); return 'rock';
-    } else {
+const getRandomThrow = (getOneTwoThree) => {
+        if (getOneTwoThree === 0) return 'rock';
+    } else if {
         (getOneTwoThree === 1) return 'paper';
     } else if {
         (getOneTwoThree === 2) return 'scissors';
     };
 
+    const getOneTwoThree = () => Math.floor(Math.random() * Math.floor(3));
 
+    console.log(getOneTwoThree);
 
 
 export default getRandomThrow;
