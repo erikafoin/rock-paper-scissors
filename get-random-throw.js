656cc65ceb9550b70a2f7aa86d37581114448080
Erika Foin <erika.foin@gmail.com>
2019-10-02 21:02:29 -0700

    JS DOM elements and initial state added

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,19 +1,17 @@
 
-const rockValue = 0;
+/*const rockValue = 0;
 const paperValue = 1;
-const scissorsValue = 2;
-
-
+const scissorsValue = 2;*/
 
 function getOneTwoThree(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-  console.log(getOneTowThree(3));
+  console.log(getOneTwoThree(3));
   
 
   const getRandomThrow = (getOneTwoThree) => {
         if (getOneTwoThree === 0); return 'rock';
-    } else if {
+    } else {
         (getOneTwoThree === 1) return 'paper';
     } else if {
         (getOneTwoThree === 2) return 'scissors';
--- r-p-s.js
+++ r-p-s.js
@@ -1,11 +1,23 @@
-import getRandomThrow from './get-random-throw.js/;
+import getRandomThrow from './get-random-throw.js/';
 
 const playButton = document.getElementById('play-button');
-const gameSummary = document.getElementById('summary');
-const gameResult = document.getElementById('result');
-const totalGames = document.getElementById('total');
-const userWins = document.getElementById('wins');
-const userLosses = document.getElementById('losses');
-const userTies = document.getElementById('ties');
+const summarySpan = document.getElementById('summary');
+const resultSpan = document.getElementById('result');
+const totalSpan = document.getElementById('total');
+const winsSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+const tiesSpan = document.getElementById('ties');
 
-console.log(getRandomThrow(3));
+let totalGames = 0;
+let tieGames = 0;
+let winGames = 0;
+let loseGames = 0;
+
+const updateSpans = () => {
+    totalSpan.textContent = totalGames;
+    winsSpan.textContent = winGames;
+    lossesSpan.textContent = loseGames;
+    tiesSpan.textContent = tieGames;
+}
+
+playButton.addEventListener('click', makeGuess);
