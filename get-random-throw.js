ff938096f668d47f82d44d3353d14a0896e47ee8
Erika Foin <erika.foin@gmail.com>
2019-10-02 17:26:26 -0700

    work started on GetRandomThrow

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,4 +1,13 @@
 
+function getOneTwoThree() {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  console.log(getOneTwoThree);
+
+
+//const getRandomThrow = () => Math.round(Math.random()) {
+
+}
 
 
 export default getRandomThrow;
--- r-p-s.js
+++ r-p-s.js
@@ -1,1 +1,9 @@
 import getThrowFromNumber from './get-random-throw.js/;
+
+const playButton = document.getElementById('play-button');
+const gameSummary = document.getElementById('summary');
+const gameResult = document.getElementById('result');
+const totalGames = document.getElementById('total');
+const userWins = document.getElementById('wins');
+const userLosses = document.getElementById('losses');
+const userTies = document.getElementById('ties');
