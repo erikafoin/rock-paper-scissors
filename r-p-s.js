917b58fe8e24e0f40be3384ae57b0edf9edc7281
Erika Foin <erika.foin@gmail.com>
2019-10-03 10:47:25 -0700

    further JS refinements but still not working

--- r-p-s.js
+++ r-p-s.js
@@ -1,7 +1,6 @@
 import getRandomThrow from './get-random-throw.js';
 
 const playButton = document.getElementById('play-button');
-const summarySpan = document.getElementById('summary');
 const resultSpan = document.getElementById('result');
 const totalSpan = document.getElementById('total');
 const winsSpan = document.getElementById('wins');
@@ -20,26 +19,52 @@ const updateSpans = () => {
     tiesSpan.textContent = tieGames;
 };
 
+const computerChoice = getRandomThrow();
+const determineWinner = (userSelectedItem, computerChoice) => {
+    if (userSelectedItem === 'rock' && computerChoice === 'rock')
+        return 'It\'s a tie!';
+    else if (userSelectedItem === 'rock' && computerChoice === 'paper')
+        return 'You lose!';   
+    else if (userSelectedItem === 'rock' && computerChoice === 'scissors')
+        return 'You win!'; 
+    else if (userSelectedItem === 'paper' && computerChoice === 'rock')
+        return 'You win!'; 
+    else if (userSelectedItem === 'paper' && computerChoice === 'paper')
+        return 'It\'s a tie!';
+    else if (userSelectedItem === 'paper' && computerChoice === 'scissors')
+        return 'You lose!';  
+    else if (userSelectedItem === 'scissors' && computerChoice === 'rock')
+        return 'You lose!'; 
+    else if (userSelectedItem === 'scissors' && computerChoice === 'paper')
+        return 'You win!'; 
+    else if (userSelectedItem === 'scissors' && computerChoice === 'scissors')
+        return 'It\'s a tie!';   
+};
+console.log(determineWinner());
+
 const makeGuess = () => {
     const selectedRadioButton = document.querySelector('input:checked');
     const userSelectedItem = selectedRadioButton.nodeValue;
     totalGames++;
 
-    const computerChoice = getRandomThrow();
+   
     const won = userWon(userSelectedItem, computerChoice);
     const tie = userTied(userSelectedItem,computerChoice);
     const lose = userLost(userSelectedItem, computerChoice);
 
-    if (won) {
-        winGames++;
-    else (tied)
-        tieGames++;
-    else (lose)
-        loseGames;
+    const updateGames = () => {
+        if (won)
+            winGames++;
+        else if (tied) 
+            tieGames++;
+        else if (lose) 
+            loseGames++;
+    };
 
     resultSpan.textContent = getRandomThrow;
     
     updateSpans();
-};
 
-playButton.addEventListener('click', makeGuess);
+
+    playButton.addEventListener('click', makeGuess);
+}
