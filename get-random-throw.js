81cdfa6ca5ac4b9235e585051fc22b018b442e3e
Erika Foin <erika.foin@gmail.com>
2019-10-02 17:54:55 -0700

    attempts at get random throw function

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,13 +1,25 @@
 
-function getOneTwoThree() {
+const rockValue = 0;
+const paperValue = 1;
+const scissorsValue = 2;
+
+
+
+function getOneTwoThree(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-  console.log(getOneTwoThree);
+  console.log(getOneTowThree(3));
+  
+
+  const getRandomThrow = (getOneTwoThree) => {
+        if (getOneTwoThree === 0); return 'rock';
+    } else if {
+        (getOneTwoThree === 1) return 'paper';
+    } else if {
+        (getOneTwoThree === 2) return 'scissors';
+    };
 
 
-//const getRandomThrow = () => Math.round(Math.random()) {
-
-}
 
 
 export default getRandomThrow;
--- r-p-s.js
+++ r-p-s.js
@@ -1,4 +1,4 @@
-import getThrowFromNumber from './get-random-throw.js/;
+import getRandomThrow from './get-random-throw.js/;
 
 const playButton = document.getElementById('play-button');
 const gameSummary = document.getElementById('summary');
@@ -7,3 +7,5 @@ const totalGames = document.getElementById('total');
 const userWins = document.getElementById('wins');
 const userLosses = document.getElementById('losses');
 const userTies = document.getElementById('ties');
+
+console.log(getRandomThrow(3));
